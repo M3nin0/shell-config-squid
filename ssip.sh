b1aa81b032c6cb56c96bfda3cf1b0c062aafd0db
Phelps <phelpsmemo@gmail.com>
2016-05-15 17:33:19 -0300

    Update ssip.sh

--- ssip.sh
+++ ssip.sh
@@ -30,19 +30,21 @@ case $menu in
 echo "Make Copies for your security"
 mv /etc/squid3/squid.conf /etc/squid3/squid.old
 # Define em qual porta o Squid vai atuar, a porta default é a 3128, mas podemos definir qualquer outra porta.
-echo "http_port 3128" >> /etc/squid3/squid.conf 
+echo "http_port 3128 transparent" >> /etc/squid3/squid.conf 
 # Define o nome do servidor
 echo "visible_hostname SegInfo" >> /etc/squid3/squid.conf
 echo "" >> /etc/squid3/squid.conf
 #Esta linha cria uma ACL, uma política de acesso com nome "all" contendo qualquer IP.
 echo "acl all src 0.0.0.0/0.0.0.0" >> /etc/squid3/squid.conf
+#Iniciando politicas de acesso
+echo "acl localhost src 127.0.0.1/255.255.255.255" >> /etc/squid3/squid.conf
 # Aqui criamos uma ACL para localhost
 
 ##########################################
 #Definindo espaços utilizados            #
 ##########################################
 
-# Memoria utilizada pelo squid
+# Memoria utilizada pelo squid 
 echo "cache_mem 128 MB" >> /etc/squid3/squid.conf
 # Limitando arquivos 
 echo "maximum_object_size_in_memory 64 KB" >> /etc/squid3/squid.conf
@@ -59,8 +61,10 @@ echo "cache_dir ufs /var/spool/squid3 2048 16 256" >> /etc/squid3/squid.conf
 #Local onde fica salvo os Logs do Squid 
 echo "cache_access_log /var/log/squid/access.log" >> /etc/squid3/squid.conf
 
-#Iniciando politicas de acesso
-echo "acl localhost src 127.0.0.1/255.255.255.255" >> /etc/squid3/squid.conf
+echo "refresh_pattern ^ftp: 15 20% 2200" >> /etc/squid3/squid.conf
+echo "refresh_pattern ^gopher: 15 0% 2280" >> /etc/squid3/squid.conf
+echo "refresh_pattern . 15 20% 2280" >> /etc/squid3/squid.conf 
+
 #Definido ACL de portas HTTPS
 echo "acl SSL_ports port 443 563" >> /etc/squid3/squid.conf
 #Definindo ACL de portas utilizadas na internet
@@ -76,6 +80,11 @@ echo "acl Safe_ports port 777 #multing HTTP" >> /etc/squid3/squid.conf
 echo "acl Safe_ports port 901 #swat" >> /etc/squid3/squid.conf
 echo "acl Safe_ports port 1025-65535 #HIGH PORTS" >> /etc/squid3/squid.conf
 
+
+##################################
+#Permissoes                      #
+##################################
+
 #Cria a ACL manager do tipo proto.
 echo "acl manager proto cache_object" >> /etc/squid3/squid.conf
 # Cria a ACL manager do tipo method.
@@ -84,45 +93,61 @@ echo "acl purge method PURGE" >> /etc/squid3/squid.conf
 echo "acl CONNECT method CONNECT" >> /etc/squid3/squid.conf
 #Libera a ACL manager e localhost.
 echo "http_access allow manager localhost" >> /etc/squid3/squid.conf
-#Bloqueia a ACL manager.
-echo "http_access deny manager" >> /etc/squid3/squid.conf
+#Libera a ACL manager.
+echo "http_access allow manager" >> /etc/squid3/squid.conf
 # Libera a ACL purge e localhost
 echo "http_access allow purge localhost" >> /etc/squid3/squid.conf
-# Bloqueia a ACL purge.
-echo "http_access deny purge" >> /etc/squid3/squid.conf
+# Libera a ACL purge.
+echo "http_access allow purge" >> /etc/squid3/squid.conf
 # Esta linha se torna bastante interessante pelo uso da "!", 
 #pois ela bloqueia qualquer conexão que não contenha o conteúdo da ACL Safe_Ports.
 echo "http_access deny !Safe_ports" >> /etc/squid3/squid.conf
 #Bloquia todas as conexões que estão fora das regras de bloqueio
- # Bloqueia qualquer conexão que não esteja no conteúdo da ACL SSL_ports.
+# Bloqueia qualquer conexão que não esteja no conteúdo da ACL SSL_ports.
 echo "http_access deny CONNECT !SSL_ports" >> /etc/squid3/squid.conf
- # Cria a ACL redelocal contendo a faixa de endereço da rede.
+#Permite acesso a tudo
+echo "http_access allow all" >> /etc/squid3/squid.conf
+echo "icp_access allow all" >> /etc/squid3/squid.conf
+
+######################################
+#Bloqueio                            #
+######################################
+
+touch /etc/squid3/sites_proibidos
+touch /etc/squid3/palavras_proibidas
+
+# Cria a ACL redelocal contendo a faixa de endereço da rede.
 echo "acl redelocal src 192.168.0.0/24" >> /etc/squid3/squid.conf
- #Libera a ACL localhost.
+#Iniciando bloqueio de sites
+echo "acl sites_proibidos url_regex -i "/etc/squid3/sites_proibidos"" >> /etc/squid3/squid.conf
+echo "http_access deny sites_proibidos" >> /etc/squid3/squid.conf
+#Libera a ACL localhost.
 echo "http_access allow localhost" >> /etc/squid3/squid.conf
 #Libera a ACL redelocal.
 echo "http_access allow redelocal" >> /etc/squid3/squid.conf
-#Bloqueia a ACL all
-echo "http_access deny all" >> /etc/squid3/squid.conf
-#Iniciando bloqueio de sites
-echo "#### Blocked ####" >> /etc/squid3/squid.conf
-#Criando arquivos de bloqueio
-touch /etc/squid3/sites_proibidos
-touch /etc/squid3/palavras_proibidas
-#Criando bloqueio de sites
-echo "acl sites_proibidos url_regex -i "/etc/squid3/sites_proibidos"" >> /etc/squid3/squid.conf
-echo "http_access deny sites_proibidos" >> /etc/squid3/squid.conf
-echo "acl palavras_proibidas dstdom_regex "/etc/squid3/palavras_proibidas"" >> /etc/squid3/squid.conf
-echo "http_access deny palavras_proibidas" >> /etc/squid3/squid.conf
-echo "### Finish Blocked  ###" >> /etc/squid3/squid.conf
-#Terminando bloqueio
+
+######################################
+#Compartilhando internet             #
+######################################
+
+modprobe iptables_nat
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE #ETH0 Representa a interface conectada a internet
+iptables -A INPUT -p tcp --syn -s 192.168.0.0/255.255.255.0 -j ACCEPT
 
 #Criando arquivo de usuario
 touch /etc/squid3/squid_passwd
 /etc/init.d/squid3 restart 
 
+#Usuario que gerencia o squid
+#Neste caso o usuario proxy
+echo "cache_effective_user proxy" >> /etc/squid3/squid.conf
+echo "cache_effective_group proxy" >> /etc/squid3/squid.conf
+useradd proxy 
+chown proxy.proxy /var/log/squid3/
+chown proxy.proxy /var/log/squid3/ 
+echo "http_access deny all" >> /etc/squid3/squid.conf
 #Iniciando instalação/configuração do Sarg
-
 apt-get install apache2 
 apt-get install sarg 
 #Entrando na pasta do Sarg 
@@ -134,6 +159,7 @@ cp sarg.conf sarg.conf.backup
 #Configurando Log do Sarg
 echo "access_log /var/log/squid3/access.log" >> sarg.conf
 echo "output_dir /var/www/html/" >> sarg.conf
+
 ;;
 
 2)
@@ -185,6 +211,22 @@ echo "iptables -A INPUT -p tcp --syn -j DROP" >> /etc/init.d/firewall.sh
 #Bloqueio de portas UDP de 0 - 65535
 iptables -A INPUT -p udp --dport 0:65535 -j DROP
 echo "iptables -A INPUT -p udp --dport 0:65535 -j DROP" >> /etc/init.d/firewall.sh
+
+#######################################
+#Fazendo proxy transparente com squid #
+#######################################
+
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3128 
+echo "iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3128 " >> /etc/init.d/firewall.sh
+
+##########################
+#Compartilhando internet #
+##########################
+
+echo "modprobe iptables_nat" >> /etc/init.d/firewall.sh
+echo "echo 1 > /proc/sys/net/ipv4/ip_forward" >> /etc/init.d/firewall.sh
+echo "iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE" >> /etc/init.d/firewall.sh
+echo "iptables -A INPUT -p tcp --syn -s 192.168.0.0/255.255.255.0 -j ACCEPT" >> /etc/init.d/firewall.sh
 ;;
 
 *)

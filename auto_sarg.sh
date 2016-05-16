a7016f5ada89f779438d58db695e16648d3cdf0b
Phelps <phelpsmemo@gmail.com>
2016-05-15 23:18:20 -0300

    Create auto_sarg.sh
    Script para a execução automatica do sarg, para que ele sempre carregue os relatorios,

---
+++ auto_sarg.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Script | Execuçao automatica Sarg
+
+#Executar a seguinte linha
+#0-59/5  * * * * /caminho_do_script/script.sh
+
+#Inserir o comando acima dentro de crontab -e
+
+
+#Executando sarg
+sarg
